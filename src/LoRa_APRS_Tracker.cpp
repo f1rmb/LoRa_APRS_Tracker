57bdadeba3df1090748960a5eeb82b6908639e24
Daniel Caujolle-Bert <f1rmb.daniel@naboo-homelinux.org>
2022-01-03 13:04:18 +0100

    Change the location of the blinking star, now it's next to the date/time. Location next to the HDOP was problematic if more than 9 satellites where spotted.

--- src/LoRa_APRS_Tracker.cpp
+++ src/LoRa_APRS_Tracker.cpp
@@ -860,24 +860,24 @@ void loop()
             }
         }
 
+        static bool blink = false; // Yeah, ugly...
         time_t n = now();
         bool posIsValid = gParams.lastValidGPS.PositionIsValid();
-        String hdopStr = (posIsValid ? String(gParams.lastValidGPS.hdop) : "--.--");
-        static bool blink = false;
+        String dtStr = (formatToDateString(n) + " " + formatToTimeString(n));
 
         if (gParams.locationFromGPS && gpsFix)
         {
             if (blink)
             {
-                hdopStr += ((hdopStr.length() == 5) ? " *" : "  *");
+                dtStr += " *";
             }
 
             blink = !blink;
         }
 
         oled.Display(bcm.getCurrentBeaconConfig()->callsign,
-                formatToDateString(n) + " " + formatToTimeString(n),
-                String("Sats: ") + (posIsValid ? (gParams.locationFromGPS ? String(gParams.lastValidGPS.satellites) : "F" ) : "-") + " HDOP: " + hdopStr,
+                dtStr,
+                String("Sats: ") + (posIsValid ? (gParams.locationFromGPS ? String(gParams.lastValidGPS.satellites) : "F" ) : "-") + " HDOP: " + (posIsValid ? String(gParams.lastValidGPS.hdop) : "--.--"),
                 String("Nxt Bcn: ") + (posIsValid ? ((gParams.locationFromGPS && bcm.getCurrentBeaconConfig()->smart_beacon.active) ? "~" : "") + formatToTimeString(gParams.nextBeaconTimeStamp) : "--:--:--"),
                 (gParams.batteryIsConnected ? (String("Bat: ") + gParams.batteryVoltage + "V, " + gParams.batteryChargeCurrent + "mA") : "Powered via USB"),
                 String("S-Beacon: " + getOnOff(gParams.locationFromGPS && bcm.getCurrentBeaconConfig()->smart_beacon.active)) + ", " + String((gParams.outputPowerWatt * 1e3), 0) + "mW");
