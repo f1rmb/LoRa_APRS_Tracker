3a9c19b7f286fd14afde9d25361985db60e08cb7
Daniel Caujolle-Bert <f1rmb.daniel@naboo-homelinux.org>
2022-01-13 11:28:23 +0100

    v0.7 and v1.x use a different pin for the user button.

--- src/Pins.h
+++ src/Pins.h
@@ -9,7 +9,14 @@
 #define OLED_SCL 22
 #define OLED_RST 16
 
-#define BUTTON_PIN 38 // The middle button GPIO on the T-Beam
+#ifdef TTGO_T_Beam_V0_7
+#define BUTTON_PIN 39 // The middle button GPIO on the T-Beam v0.7
+#endif
+
+#ifdef TTGO_T_Beam_V1_0
+#define BUTTON_PIN 38 // The middle button GPIO on the T-Beam v1.x
+#endif
+
 #define SERIAL0_RX_GPIO 3
 
 #ifdef TTGO_T_Beam_V0_7
